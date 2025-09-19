149ebbcd6ee86e9a4ba697fc970fffbe4e518ceb
Nandkishor Nelekar <168915841+NandkishorNelekar@users.noreply.github.com>
2025-09-19 11:47:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll(".section");
+const revealOnScroll = () => {
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+};
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
